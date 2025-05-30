b7bfdf03b51daeee313f28fc070a559ea1f16405
Taraaulya19 <taraaulia91@gmail.com>
2025-05-30 07:56:12 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Layout from "./components/Layout"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
